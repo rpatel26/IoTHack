8148904b3df0f27a21a3f9883233e9acf63d4ee8
skomatin <saikiran@usc-guestwireless-upc-newsc12487.usc.edu>
2019-03-24 11:09:36 -0700

    object detection done

---
+++ two.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+
+def detectObject(img):
+    #file_Path = "./train.jpg"
+
+    #img = cv2.imread(file_Path, 0)
+    img = img[:,0:3024]
+    img = cv2.resize(img, (320,320))
+    imgShape = img.shape
+    # print(imgShape)
+    count = 0
+    set = False
+    for i in range(imgShape[0]):
+        xArr = img[i,:]
+        fx = np.array(xArr, dtype = float)
+        fx = np.gradient(fx)
+        for j in range(imgShape[1]):
+          if img[i,j] > abs(20):
+              if count >= 30:
+                  print("done!")
+                  finalPosx = i
+                  finalPosy = j
+                  set = True
+                  break
+
+              count = 0
+              continue
+              #first = True
+          else:
+              count+=1
+        if set:
+            print(finalPosx)
+            print(finalPosy)
+            break
+
+    returnVal = finalPosy/320
+    return returnVal
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
